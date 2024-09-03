2867af10b87ecf3b199ee7d46fbc7060218f7c6f
Arran Schlosberg <me@arranschlosberg.com>
2024-09-03 21:37:53 +0100

    fix: `StateTransition.canExecuteTransaction()` used `msg.From` instead of `To`

--- core/state_transition.libevm.go
+++ core/state_transition.libevm.go
@@ -3,5 +3,5 @@ package core
 func (st *StateTransition) canExecuteTransaction() error {
 	bCtx := st.evm.Context
 	rules := st.evm.ChainConfig().Rules(bCtx.BlockNumber, bCtx.Random != nil, bCtx.Time)
-	return rules.Hooks().CanExecuteTransaction(st.msg.From, st.msg.From, st.state)
+	return rules.Hooks().CanExecuteTransaction(st.msg.From, st.msg.To, st.state)
 }
