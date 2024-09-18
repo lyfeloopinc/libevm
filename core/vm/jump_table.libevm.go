263a9cbda40782cda9cb2e4797d160a6c3e7dfbf
Arran Schlosberg <me@arranschlosberg.com>
2024-09-18 10:09:33 -0400

    chore: include changes forgotten in last commit

--- core/vm/jump_table.libevm.go
+++ core/vm/jump_table.libevm.go
@@ -1,20 +1,5 @@
 package vm
 
-import "github.com/ethereum/go-ethereum/params"
-
-// overrideJumpTable returns `libevmHooks.OverrideJumpTable(r,jt)“ i.f.f. the
-// Rules' hooks indicate that it must, otherwise it echoes `jt` unchanged.
-func overrideJumpTable(r params.Rules, jt *JumpTable) *JumpTable {
-	if !r.Hooks().OverrideJumpTable() {
-		return jt
-	}
-	// We don't check that libevmHooks is non-nil because the user has clearly
-	// signified that they want an override. A nil-pointer dereference will
-	// occur in tests whereas graceful degradation would frustrate the user with
-	// a hard-to-find bug.
-	return libevmHooks.OverrideJumpTable(r, jt)
-}
-
 // An OperationBuilder is a factory for a new operations to include in a
 // [JumpTable].
 type OperationBuilder struct {
@@ -57,23 +42,4 @@ func (fn OperationFunc) internal() executionFunc {
 			}, pc, interpreter, scope,
 		)
 	}
-}
-
-// Hooks are arbitrary configuration functions to modify default VM behaviour.
-type Hooks interface {
-	// OverrideJumpTable will only be called if
-	// [params.RulesHooks.OverrideJumpTable] returns true. This allows for
-	// recursive calling into [LookupInstructionSet].
-	OverrideJumpTable(params.Rules, *JumpTable) *JumpTable
-}
-
-var libevmHooks Hooks
-
-// RegisterHooks registers the Hooks. It is expected to be called in an `init()`
-// function and MUST NOT be called more than once.
-func RegisterHooks(h Hooks) {
-	if libevmHooks != nil {
-		panic("already registered")
-	}
-	libevmHooks = h
 }
